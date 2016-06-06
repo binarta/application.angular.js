6309894dd2620d2eaf01132cc3ae6013331a6396
Jo Kerkhofs <jke@thinkerit.be>
2016-06-06 14:30:10 +0200

    Manage angular dependency through angularx module. Define sources.

--- test/application.spec.js
+++ test/application.spec.js
@@ -14,43 +14,11 @@ describe('application', function() {
     describe('applicationDataService', function() {
         var service, commonApplicationDataDeferred;
 
-        beforeEach(inject(function(_applicationDataService_, $q, $rootScope) {
+        beforeEach(inject(function(_applicationDataService_, $q) {
             service = _applicationDataService_;
             commonApplicationDataDeferred = $q.defer();
-            var d = commonApplicationDataDeferred.resolve;
-            commonApplicationDataDeferred.resolve = function(args) {
-                d(args);
-                $rootScope.$digest();
-            };
             rest.and.returnValue(commonApplicationDataDeferred.promise);
         }));
-
-        describe('then', function() {
-            var config;
-
-            beforeEach(function() {
-                service.then(function(it) {
-                    config = it;
-                });
-            });
-
-            it('common data is retrieved', inject(function(config) {
-                expect(rest.calls.argsFor(0)[0].params).toEqual({
-                    method:'GET',
-                    url:config.baseUri + 'api/application/' + config.namespace + '/data/common'
-                })
-            }));
-
-            it('subsequent calls do not perform additional rest calls', function() {
-                service.then(function() {});
-                expect(rest.calls.count()).toEqual(1);
-            });
-
-            it('when app is trial', inject(function($rootScope) {
-                commonApplicationDataDeferred.resolve({data: {k:'v'}});
-                expect(config).toEqual({k:'v'});
-            }));
-        });
 
         describe('isTrial', function() {
             var isTrial;
@@ -62,10 +30,10 @@ describe('application', function() {
             });
 
             it('common data is retrieved', inject(function(config) {
-                expect(rest.calls.argsFor(0)[0].params).toEqual({
+                expect(rest.calls.first().args[0].params).toEqual({
                     method:'GET',
                     url:config.baseUri + 'api/application/' + config.namespace + '/data/common'
-                })
+                });
             }));
 
             it('subsequent calls do not perform additional rest calls', function() {
@@ -75,11 +43,13 @@ describe('application', function() {
 
             it('when app is trial', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data: {trial:{}}});
+                $rootScope.$digest();
                 expect(isTrial).toBeTruthy();
             }));
 
             it('when app is not a trial', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data:{}});
+                $rootScope.$digest();
                 expect(isTrial).toBeFalsy();
             }));
         });
@@ -94,7 +64,7 @@ describe('application', function() {
             });
 
             it('common data is retrieved', inject(function(config) {
-                expect(rest.calls.argsFor(0)[0].params).toEqual({
+                expect(rest.calls.first().args[0].params).toEqual({
                     method:'GET',
                     url:config.baseUri + 'api/application/' + config.namespace + '/data/common'
                 })
@@ -102,21 +72,24 @@ describe('application', function() {
 
             it('subsequent calls do not perform additional rest calls', function() {
                 service.isTrial();
-                expect(rest.calls[1]).toBeUndefined();
+                expect(rest.calls.count()).toEqual(1);
             });
 
             it('when app is not a trial', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data: {}});
+                $rootScope.$digest();
                 expect(isExpired).toBeFalsy();
             }));
 
             it('when app is a trial and not expired', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data:{trial:{expired:false}}});
+                $rootScope.$digest();
                 expect(isExpired).toBeFalsy();
             }));
 
             it('when app is a trial and expired', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data:{trial:{expired:true}}});
+                $rootScope.$digest();
                 expect(isExpired).toBeTruthy();
             }));
         });
@@ -131,7 +104,7 @@ describe('application', function() {
             });
 
             it('common data is retrieved', inject(function(config) {
-                expect(rest.calls.argsFor(0)[0].params).toEqual({
+                expect(rest.calls.first().args[0].params).toEqual({
                     method:'GET',
                     url:config.baseUri + 'api/application/' + config.namespace + '/data/common'
                 })
@@ -139,17 +112,19 @@ describe('application', function() {
 
             it('subsequent calls do not perform additional rest calls', function() {
                 service.isTrial();
-                expect(rest.calls[1]).toBeUndefined();
+                expect(rest.calls.count()).toEqual(1);
             });
 
             it('when app is not a trial', inject(function($rootScope) {
                 commonApplicationDataDeferred.resolve({data: {}});
+                $rootScope.$digest();
                 expect(expirationDate).toBeFalsy();
             }));
 
             it('when app is a trial', inject(function($rootScope) {
                 var now = moment();
                 commonApplicationDataDeferred.resolve({data:{trial:{expirationDate:now.toISOString()}}});
+                $rootScope.$digest();
                 expect(expirationDate.toObject()).toEqual(now.toObject());
             }));
         });
